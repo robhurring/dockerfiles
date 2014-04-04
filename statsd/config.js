bc0f015538a1a90783362632ced5d9c5dced23f4
robhurring <robhurring@gmail.com>
2014-04-04 15:32:56 -0400

    statsd

---
+++ statsd/config.js
@@ -0,0 +1,12 @@
+(function() {
+  return {
+    graphitePort: Number(process.env.GRAPHITE_PORT) || 2003,
+    graphiteHost: process.env.GRAPHITE_HOST || "localhost",
+    port: Number(process.env.STATSD_PORT) || 8125,
+    backends: [ "./backends/graphite" ],
+    debug: !!process.env.DEBUG,
+    dumpMessages: !!process.env.DEBUG,
+    flushInterval: 1
+  };
+})();
+
